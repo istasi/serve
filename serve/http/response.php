81c114092be783347ff98ac67e3e741a14be8ed0
istasi <istasi@gmail.com>
2023-07-07 16:04:07 -0400

    remade to follow same state system as request

--- serve/http/response.php
+++ serve/http/response.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace serve\http;
 
+define ('SERVE_HTTP_RESPONSE_WAITING', 1);
+define ('SERVE_HTTP_RESPONSE_HEADERS', 2);
+define ('SERVE_HTTP_RESPONSE_BODY', 3);
+define ('SERVE_HTTP_RESPONSE_COMPLETE', 4);
+
 class response
 {
+	private int $state;
+
+	private int $code;
+	private array $codeMap = [];
+
 	private array $headers = [];
 	private string $response;
 
@@ -14,18 +24,30 @@ class response
 	public function __construct ( readonly public request $request, readonly private \serve\connections\connection $connection )
 	{
 		$this->reset ();
+
+		$this->codeMap = [
+			200 => 'OK',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			400 => 'Bad Request',
+			404 => 'Not Found'
+		];
+
+		$this->code = 200;
 	}
 
 	public function reset (): void
 	{
+		$this->state = SERVE_HTTP_RESPONSE_WAITING;
 		$this->sent = false;
 
 		$this->headers = [];
-		$this->response = 'HTTP/1.1 200 OK'. "\r\n";
+		$this->response = ''; // HTTP/1.1 200 OK'. "\r\n";
 
-		$this->header ('Content-Type', 'text/html');
-		$this->header ('Connection', 'keep-alive');
-		$this->header ('Date', date ('c'));
+		$this->header ('content-encoding', 'gzip');
+		$this->header ('content-type', 'text/html');
+		$this->header ('connection', 'keep-alive');
+		$this->header ('date', date ('c'));
 	}
 
 	public function sent (): bool
@@ -43,65 +65,67 @@ class response
 		return true;
 	}
 
-	public function body ( string $message ): bool
+	public function redirect ( string $url, int $code = 302 ): void
 	{
-		if ( empty ( $this->headers ['content-encoding'] ) === true )
-			$this->header ('content-encoding', 'gzip');
-
-		if ( empty ( $this->headers['content-encoding'] ) === false )
-		{
-			$accept = $this->request->header ('accept-encoding');
-			if ( !$accept )
-				$accept = '';
-			$accept = str_replace (' ', '', $accept );
-			$accept = explode (',', $accept );
-			if ( !in_array ( haystack: $accept, needle: 'gzip' ) )
-				unset ( $this->headers ['content-encoding'] );
-		}
-
-		switch ( $this->headers ['content-encoding'] ?? '' )
-		{
-			case 'deflate':
-				$message = gzdeflate ( $message, 9 );
-				break;
-			case 'gzip':
-				$message = gzencode ( $message );
-				break;
-			default:
-				unset ( $this->headers ['content-encoding'] );
-				break;
-		}
-
-		if ( $this->sent === false )
-		{
-			foreach ( $this->headers as $key => $value )
-				$this->response .= $key .': '. $value ."\r\n";
-
-			$this->response .= 'Content-Length: '. strlen ( $message ) ."\r\n";
-			$this->response .= "\r\n";
-		}
-			
-		$this->sent = true;
-		$this->response .= $message;
-
-		return true;
+		$this->code = $code;
+		$this->header ('location', $url);
 	}
 
-	public function send ( string $body = null ): bool
+	public function send ( string $content = null, int $code = null ): bool
 	{
-		if ( !$this->request->complete() )
-			return false;
+		$raw = '';
 
-		if ( $body )
-			$this->body ( $body );
+		switch ( $this->state )
+		{
+			case SERVE_HTTP_RESPONSE_WAITING:
+				$raw .= 'HTTP/1.1 '. $this->code .' '. $this->codeMap [ $this->code ] . "\r\n";
+				$this->state = SERVE_HTTP_RESPONSE_HEADERS;
+			case SERVE_HTTP_RESPONSE_HEADERS:
+				$length = strlen ( $content );
+				if ( $length > 1024 * 10 )
+					$this->headers ['content-encoding'] = 'deflate';
+				else if ( $length > 1024 )
+					$this->headers ['content-encoding'] = 'gzip';
+				else
+					unset ( $this->headers ['content-encoding'] );
 
-		if ( empty ( $this->response ) )
-			return false;
+				if ( empty ( $this->headers ['content-encoding'] ) === false )
+				{
+					$accept = $this->request->header ('accept-encoding');
+					if ( $accept )
+					{
+						$accept = str_replace (' ', '', $accept);
+						$bits = explode (',', $accept );
+
+						if ( in_array ( haystack: $bits, needle: $this->headers ['content-encoding'] ) === false )
+							$this->header ('content-encoding', null);
+					}
+
+					switch ( $this->headers ['content-encoding'] )
+					{
+						case 'gzip':
+							$content = gzencode($content);
+							break;
+						case 'deflate':
+							$content = gzdeflate($content, 9);
+							break;
+					}
+				}
+
+				$this->header ('content-length', (string) strlen ($content));
+
+				foreach ( $this->headers as $key => $value )
+					if ( $value )
+						$raw .= $key .': '. $value ."\r\n";
+
+				$this->state = SERVE_HTTP_RESPONSE_BODY;
+			case SERVE_HTTP_RESPONSE_BODY:
+				$raw .= "\r\n". $content;
+				$this->state = SERVE_HTTP_RESPONSE_COMPLETE;
+		}
 
 		$this->sent = true;
-
-		$this->connection->write ( $this->response );
-		$this->response = '';
+		$this->connection->write ( $raw );
 
 		$this->reset ();
 		$this->request->reset ();
