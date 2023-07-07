53b0dcb662d87962223f04f46aba1e37ad70500c
istasi <istasi@gmail.com>
2023-07-07 06:19:30 -0400

    spelling error

--- site/main.php
+++ site/main.php
@@ -7,4 +7,4 @@ require_once ('test.php');
 if ( !showcase::$variable )
 	showcase::$variable = date ('c');
 
-$response->send ('<h1>Hello worlds</h1><h4>'. date ('c') .'</h4><h4>'. showcase::$variable .'</h4><pre>'. showcase () .'</pre>');
+$response->send ('<h1>Hello world</h1><h4>'. date ('c') .'</h4><h4>'. showcase::$variable .'</h4><pre>'. showcase () .'</pre>');
