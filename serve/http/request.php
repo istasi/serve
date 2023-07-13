247b01b98711a4660bfb84580843a21a5598d58b
istasi <istasi@gmail.com>
2023-07-12 20:15:57 -0400

    trim cookie name, removing posible leading whitespace

--- serve/http/request.php
+++ serve/http/request.php
@@ -6,118 +6,118 @@ namespace serve\http;
 
 class request
 {
-    private bool $locked = false;
-    private array $server;
-    private array $headers;
-    private array $content;
+	private bool $locked = false;
+	private array $server;
+	private array $headers;
+	private array $content;
 
-    public function __construct()
-    {
+	public function __construct()
+	{
 
-    }
-/*
-    public function __debugInfo()
-    {
-        return [
-            'server' => $this->server,
-            'headers' => $this->headers,
-            'content' => $this->content
-        ];
-    }
-*/
+	}
+	/*
+		public function __debugInfo()
+		{
+			return [
+				'server' => $this->server,
+				'headers' => $this->headers,
+				'content' => $this->content
+			];
+		}
+	*/
 
-    public function lock(): void
-    {
-        $this->locked = true;
+	public function lock(): void
+	{
+		$this->locked = true;
 
-        if (isset($this->content) === false) {
-            $this->content = [];
-        }
-    }
+		if (isset($this->content) === false) {
+			$this->content = [];
+		}
+	}
 
-	public function unlock (): void
+	public function unlock(): void
 	{
 		$this->locked = false;
 	}
 
-    public function __server(array $content): void
-    {
-        $this->server = $content;
-    }
+	public function __server(array $content): void
+	{
+		$this->server = $content;
+	}
 
-    public function __headers(array $content): void
-    {
-        if ($this->locked) {
-            return;
-        }
+	public function __headers(array $content): void
+	{
+		if ($this->locked) {
+			return;
+		}
 
-        $this->headers = $content;
-    }
+		$this->headers = $content;
+	}
 
-    public function __content(array $content): void
-    {
-        if ($this->locked) {
-            return;
-        }
+	public function __content(array $content): void
+	{
+		if ($this->locked) {
+			return;
+		}
 
-        $this->content = $content;
-    }
+		$this->content = $content;
+	}
 
-    public function __get($key): mixed
-    {
-        switch ($key) {
-            case 'server':
-            case 'headers':
-            case 'content':
-                return $this->{$key};
-        }
-    }
+	public function __get($key): mixed
+	{
+		switch ($key) {
+			case 'server':
+			case 'headers':
+			case 'content':
+				return $this->{$key};
+		}
+	}
 
-    public function header(string $key): mixed
-    {
-        if (isset($this->headers [$key]) === false) {
-            return null;
-        }
+	public function header(string $key): mixed
+	{
+		if (isset($this->headers [$key]) === false) {
+			return null;
+		}
 
-        return $this->headers [ $key ];
-    }
+		return $this->headers [ $key ];
+	}
 
-    public function server(string $key): mixed
-    {
-        if (isset($this->server [$key]) === false) {
-            return null;
-        }
+	public function server(string $key): mixed
+	{
+		if (isset($this->server [$key]) === false) {
+			return null;
+		}
 
-        return $this->server [$key];
-    }
+		return $this->server [$key];
+	}
 
-    private bool $processedCookies = false;
-    private array $cookies = [];
-    public function cookie(string $key): mixed
-    {
-        if ($this->processedCookies === false);
-        {
-            $cookies = $this->header('cookie');
-            if ($cookies === null) {
-                return null;
-            }
+	private bool $processedCookies = false;
+	private array $cookies = [];
+	public function cookie(string $key): mixed
+	{
+		if ($this->processedCookies === false);
+		{
+			$cookies = $this->header('cookie');
+			if ($cookies === null) {
+				return null;
+			}
 
-            $cookies = explode(';', $cookies);
-            foreach ($cookies as $cookie) {
-                $bits = explode('=', $cookie, 2);
-                if (isset($bits [1]) === false) {
-                    $bits [1] = null;
-                }
+			$cookies = explode(';', $cookies);
+			foreach ($cookies as $cookie) {
+				$bits = explode('=', $cookie, 2);
+				if (isset($bits [1]) === false) {
+					$bits [1] = null;
+				}
 
-                $this->cookies [ $bits [0] ] = $bits [1];
-            }
-            $this->processedCookies = true;
-        }
+				$this->cookies [trim($bits [0])] = $bits [1];
+			}
+			$this->processedCookies = true;
+		}
 
-        if (isset($this->cookies [$key]) === false) {
-            return null;
-        }
+		if (isset($this->cookies [$key]) === false) {
+			return null;
+		}
 
-        return $this->cookies [$key];
-    }
+		return $this->cookies [$key];
+	}
 }
