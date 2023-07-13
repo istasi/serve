5528f58eb0a8f11fd3f4588e645bb4c9f0143936
istasi <istasi@gmail.com>
2023-07-13 15:09:13 -0400

    Allow most easier customization

--- serve/log.php
+++ serve/log.php
@@ -6,11 +6,40 @@ namespace serve;
 
 class log
 {
-	static public int $id = 0;
-	static public $fp = STDOUT;
-	
-	static public function entry ( $message ): void
+	public static int $id = 0;
+
+	/**
+	 * static public \resource $fp = STDOUT;
+	 * PHP Fatal error: Uncaught TypeError: Cannot assign resource to property serve\log::$fp of type resource in
+	 */
+	public static $fp = STDOUT;
+
+	/**
+	 * Why php, why?
+	 * it does not make sense to me when you can typehint it in function arguments function ( callable $parser )
+	 * so clearly you can say a variable should be of a type, why should it matter where that variable exists
+	 * 
+	 * static public callable $parser;
+	 * PHP Fatal error:  Property serve\log::$parser cannot have type callable in
+	 */
+	private static $__parser = null;
+	public static function parser(callable $parser): void
 	{
-		fwrite (self::$fp, '['. self::$id .']['. date ('c') .'] '. $message. PHP_EOL);
+		self::$__parser = $parser;
+	}
+
+	public static function entry($message): void
+	{
+		/**
+		 * PHP Fatal error:  Uncaught Error: Method name must be a string in
+		 * fwrite(self::$fp, self::$parser(). $message. PHP_EOL);
+		 */
+
+		$parser = self::$__parser;
+		fwrite(self::$fp, $parser(). $message. PHP_EOL);
 	}
 }
+
+log::parser(function () {
+	return '['. log::$id .']['. date('c') .'] ';
+});
